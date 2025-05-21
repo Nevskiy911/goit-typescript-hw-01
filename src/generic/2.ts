1c15524e2bb53664e1017e818c41429bdc19c5ba
Nevskiy911 <maleev1820@gmail.com>
2025-05-21 05:32:29 +0200

    fixed all

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -5,14 +5,14 @@ type AllType = {
   weight: number;
 };
 
-function compare<T extends Partial<AllType>, U extends Partial<AllType>>(
-  top: T,
-  bottom: U
-): AllType {
+function compare<
+  T extends Pick<AllType, "name" | "color">,
+  U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllType {
   return {
-    name: top.name as string,
-    color: top.color as string,
-    position: bottom.position as number,
-    weight: bottom.weight as number,
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
   };
 }
